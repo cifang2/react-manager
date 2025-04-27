9125d0ba87a958e84a39b3f318e879d521f4f890
CiFang <2628201082@qq.com>
2025-04-27 22:25:30 +0800

    feat: 配置vite别名和压缩功能

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,57 @@
-import { ConfigEnv, defineConfig, UserConfig } from 'vite'
+import { ConfigEnv, defineConfig, loadEnv, UserConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
+import { warppEnv } from './src/utile/getEnv'
+import viteCompression from 'vite-plugin-compression'//压缩的包
 
 // https://vite.dev/config/
 export default defineConfig((mode: ConfigEnv): UserConfig => {
+  const env = loadEnv(mode.mode, process.cwd())
+  const viteEnv = warppEnv(env)
   // 读取配置文件
   return {
-    plugins: [react()],
+    plugins: [react(),
+    viteEnv.VITE_COMPRESS && viteCompression({
+      verbose: true,
+      disable: false,
+      threshold: 10240,
+      algorithm: 'gzip',
+      ext: '.gz',
+      deleteOriginFile: false,
+      filter: /\.(js|mjs|json|css|html)$/,
+    }),//配置打包压缩
+    ],
     resolve: {
       alias: {
         '@': resolve(__dirname, 'src')
       }
     },
-    base: mode.command === 'serve' ? '/' : './'
+    server: {//这里直接使用env配置
+      host: '0.0.0.0',
+      port: viteEnv.VITE_PORT,
+      open: viteEnv.VITE_OPEN,
+      proxy: {
+        // '/api': {
+        //   target: viteEnv.VITE_PROXY,
+        //   changeOrigin: true,
+        //   rewrite: (path) => path.replace(/^\/api/, '')
+        // }
+      }
+    },
+
+    base: mode.command === 'serve' ? '/' : './',
+    build: {
+      outDir: 'dist',//打包后的目录
+      minify: 'esbuild',//打包的方式，支持esbuild和terser，
+      //minify:'terser',
+      //teser可以去除console
+      rollupOptions: {
+        output: {
+          chunkFileNames: 'assets/js/[name]-[hash].js',
+          entryFileNames: 'assets/js/[name]-[hash].js',
+          assetFileNames: 'assets/[ext]/[name]-[hash].[ext]',
+        },
+      }
+    }
   }
 })
