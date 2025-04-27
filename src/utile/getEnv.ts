ad6120fde7e3ab7c9e4469f1d60ee02f6c5458bf
CiFang <2628201082@qq.com>
2025-04-27 22:25:29 +0800

    feat: 添加环境变量处理工具

--- src/utile/getEnv.ts
+++ src/utile/getEnv.ts
@@ -1,1 +1,23 @@
-//加载Env的的函数
+
+
+//加载Env配置文件的的函数 
+export function warppEnv(envConf) {
+    const ret: any = {};
+    for (const envName of Object.keys(envConf)) {
+        let realName = envConf[envName].replace(/\\n/g, '\n');
+        realName = realName === 'true' ? true : realName === 'false' ? false : realName;
+        if (envName === 'VITE_PORT') {
+            realName = Number(realName);
+        }
+        if (envName === 'VITE_PROXY') {
+            try {
+                realName = JSON.parse(realName.replace(/'/g, '"'));
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        ret[envName] = realName;
+        process.env[envName] = realName;
+    }
+    return ret;
+}
