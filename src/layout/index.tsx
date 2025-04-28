20dee0e694dd55ede7b678aa050e23caeb0a1566
CiFang <2628201082@qq.com>
2025-04-28 12:02:56 +0800

    feat: 创建基础布局组件

---
+++ src/layout/index.tsx
@@ -0,0 +1,26 @@
+import { Layout } from 'antd'
+import Sider from 'antd/es/layout/Sider'
+import { Outlet } from 'react-router-dom'
+//基于antd的布局
+const layoutIndex = () => {
+    return (
+        <div className="container">
+            <Sider collapsible width={220} theme="dark" >
+                Sider
+            </Sider>
+            <Layout>
+                <Layout.Header>
+                    Header
+                </Layout.Header>
+                <Layout.Content>
+                    <Outlet />
+                </Layout.Content>
+                <Layout.Footer>
+                    Footer
+                </Layout.Footer>
+            </Layout>
+        </div>
+    )
+}
+
+export default layoutIndex
