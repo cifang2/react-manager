0f6780321ab148d5c98ac03a70fa0442832a9f47
CiFang <2628201082@qq.com>
2025-04-28 17:16:02 +0800

    添加 Logo 组件，使用 @/assets/react.svg 作为图标

---
+++ src/layout/components/components/logo.tsx
@@ -0,0 +1,9 @@
+import logo from "@/assets/react.svg"
+const Logo = () => {
+    return (
+        <div>
+            <img src={logo} alt="logo"></img>
+        </div>
+    )
+}
+export default Logo
