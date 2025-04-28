a3ac7f647b83dad1f3c1c440021ff95914085299
CiFang <2628201082@qq.com>
2025-04-28 12:20:43 +0800

    chore: 添加less依赖并引入样式文件
    添加less依赖以支持样式预处理器，并在布局组件中引入index.less文件。同时，调整了index.css中的样式以优化布局。

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,6 +1,7 @@
 import { Layout } from 'antd'
 import Sider from 'antd/es/layout/Sider'
 import { Outlet } from 'react-router-dom'
+import "./index.less"
 //基于antd的布局
 const layoutIndex = () => {
     return (
