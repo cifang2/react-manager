88ba42c1abb4e65aec82c9dcc8e52dc354296e0e
CiFang <2628201082@qq.com>
2025-05-08 23:05:21 +0800

    feat: 集成Redux和持久化存储以管理全局状态
    在main.tsx中引入Redux的Provider和PersistGate，用于全局状态管理和持久化存储。在login/index.tsx中，通过connect将setToken action映射到props，并实现登录功能，点击按钮后设置token并跳转到首页。

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { Provider } from 'react-redux'
+import { store, persistor } from '@/redux/'
 import App from './App.tsx'
+import { PersistGate } from 'redux-persist/integration/react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </StrictMode>,
 )
--- src/views/login/index.tsx
+++ src/views/login/index.tsx
@@ -1,9 +1,33 @@
-const Login = () => {
+import { Button } from "antd"
+import { setToken } from "@/redux/modules/global/action"
+import { connect } from "react-redux"
+import { useNavigate } from "react-router-dom"
+
+interface LoginProps {
+    setToken: typeof setToken
+}
+
+
+const Login = (props: LoginProps) => {
+    const { setToken } = props
+    const navigate = useNavigate()
+    const handleLogin = () => {
+        console.log('login')
+        setToken('token')
+        navigate('/sys/home')
+    }
+
     return (
         <div>
-            Login
+            <Button type="primary" onClick={handleLogin}>Login</Button>
         </div>
     )
 }
 
-export default Login
+//export default Login
+
+//映射dispatch到props上
+const mapDispatchToProps = {
+    setToken
+}
+export default connect(null, mapDispatchToProps)(Login)
