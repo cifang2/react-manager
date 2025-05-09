89900867647cd2a9bf83d03e2797c893de79be8a
CiFang <2628201082@qq.com>
2025-05-09 16:32:40 +0800

    feat(layout): 添加未登录用户重定向到登录页功能
    从redux store中获取token，如果未登录则重定向到登录页

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,10 +1,16 @@
 import { Layout } from 'antd'
 import Sider from 'antd/es/layout/Sider'
-import { Outlet } from 'react-router-dom'
+import { Navigate, Outlet } from 'react-router-dom'
 import "./index.less"
 import LayoutMenu from './components/Menu'
+import { store } from '@/redux'
 //基于antd的布局
 const layoutIndex = () => {
+    //从store中获取token
+    const userToken = store.getState().global.token
+    if (!userToken) {
+        return <Navigate to="/login" />
+    }
     return (
         <div className="container">
             <Sider collapsible width={220} theme="dark" >
